5766bdc17eff9504cf5fdad4722bf0d6663f9549
vaneycksr <vaneyck23@gmail.com>
2020-08-15 13:55:07 -0300

    testes unitarios da aula da pos graduacao

---
+++ calculadoradedescontos/src/test/java/br/unipe/allsolution/calculadoradedescontos/CalculadoradedescontosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unipe.allsolution.calculadoradedescontos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoradedescontosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ calculadoradedescontos/src/test/java/br/unipe/allsolution/calculadoradedescontos/model/TipoClienteTest.java
@@ -0,0 +1,124 @@
+package br.unipe.allsolution.calculadoradedescontos.model;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TipoClienteTest {
+
+    TipoCliente driver;
+
+    Integer quantidadeInferiorA1000, quantidadeInferiorA100, quantidadeAPartirDe1000;
+
+    @BeforeEach
+    public void setUp() {
+        this.quantidadeInferiorA100 = 99;
+        this.quantidadeInferiorA1000 = 999;
+        this.quantidadeAPartirDe1000 = 1000;
+    }
+
+    @AfterEach
+    public void tearDown() {
+        this.driver = null;
+    }
+
+    @Test
+    @Tags({@Tag("All"),@Tag("ClienteA")})
+    void testGetFatorDescontoQuandoClienteAQuantidadeInferiorA100EntaoFatorSera090() {
+
+        this.driver = TipoCliente.A;
+
+        Double resultado = this.driver.getFatorDesconto(this.quantidadeInferiorA100);
+
+        assertEquals(0.90,resultado);
+    }
+
+    @Test
+    @Tags({@Tag("All"),@Tag("ClienteA")})
+    void testGetFatorDescontoQuandoClienteAQuantidadeInferiorA1000EntaoFatorSera095() {
+
+        this.driver = TipoCliente.A;
+
+        Double resultado = this.driver.getFatorDesconto(this.quantidadeInferiorA1000);
+
+        assertEquals(0.95,resultado);
+    }
+
+    @Test
+    @Tags({@Tag("All"),@Tag("ClienteA")})
+    void testGetFatorDescontoQuandoClienteAQuantidadeAPartirDe1000EntaoFatorSera100() {
+
+        this.driver = TipoCliente.A;
+
+        Double resultado = this.driver.getFatorDesconto(this.quantidadeAPartirDe1000);
+
+        assertEquals(1.0,resultado);
+    }
+
+    @Test
+    @Tags({@Tag("All"),@Tag("ClienteB")})
+    void testGetFatorDescontoQuandoClienteBQuantidadeInferiorA100EntaoFatorSera085() {
+
+        this.driver = TipoCliente.B;
+
+        Double resultado = this.driver.getFatorDesconto(this.quantidadeInferiorA100);
+
+        assertEquals(0.85,resultado);
+    }
+
+    @Test
+    @Tags({@Tag("All"),@Tag("ClienteB")})
+    void testGetFatorDescontoQuandoClienteBQuantidadeInferiorA1000EntaoFatorSera090() {
+
+        this.driver = TipoCliente.B;
+
+        Double resultado = this.driver.getFatorDesconto(this.quantidadeInferiorA1000);
+
+        assertEquals(0.90,resultado);
+    }
+
+    @Test
+    @Tags({@Tag("All"),@Tag("ClienteB")})
+    void testGetFatorDescontoQuandoClienteBQuantidadeAPartirDe1000EntaoFatorSera095() {
+
+        this.driver = TipoCliente.B;
+
+        Double resultado = this.driver.getFatorDesconto(this.quantidadeAPartirDe1000);
+
+        assertEquals(0.95,resultado);
+    }
+
+    @Test
+    @Tags({@Tag("All"),@Tag("ClienteC")})
+    void testGetFatorDescontoQuandoClienteBQuantidadeInferiorA100EntaoFatorSera080() {
+
+        this.driver = TipoCliente.C;
+
+        Double resultado = this.driver.getFatorDesconto(this.quantidadeInferiorA100);
+
+        assertEquals(0.80,resultado);
+    }
+
+    @Test
+    @Tags({@Tag("All"),@Tag("ClienteC")})
+    void testGetFatorDescontoQuandoClienteBQuantidadeInferiorA1000EntaoFatorSera085() {
+
+        this.driver = TipoCliente.C;
+
+        Double resultado = this.driver.getFatorDesconto(this.quantidadeInferiorA1000);
+
+        assertEquals(0.85,resultado);
+    }
+
+    @Test
+    @Tags({@Tag("All"),@Tag("ClienteC")})
+    void testGetFatorDescontoQuandoClienteBQuantidadeAPartirDe1000EntaoFatorSera090() {
+
+        this.driver = TipoCliente.C;
+
+        Double resultado = this.driver.getFatorDesconto(this.quantidadeAPartirDe1000);
+
+        assertEquals(0.90,resultado);
+    }
+
+}
