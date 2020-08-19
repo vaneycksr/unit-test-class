8478ab6c4394f11f527637b065a603c6602436be
vaneycksr <vaneyck23@gmail.com>
2020-08-18 22:59:11 -0300

    implementando testes de integracao usando mockito

--- calculadoradedescontos/src/test/java/br/unipe/allsolution/calculadoradedescontos/service/ItemServiceMockTest.java
+++ calculadoradedescontos/src/test/java/br/unipe/allsolution/calculadoradedescontos/service/ItemServiceMockTest.java
@@ -1,6 +1,9 @@
 package br.unipe.allsolution.calculadoradedescontos.service;
 
-import org.junit.jupiter.api.Test;
+import br.unipe.allsolution.calculadoradedescontos.model.Item;
+import br.unipe.allsolution.calculadoradedescontos.model.Produto;
+import br.unipe.allsolution.calculadoradedescontos.model.TipoCliente;
+import com.github.javafaker.Faker;
 import br.unipe.allsolution.calculadoradedescontos.repository.ItemRepository;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,12 +13,13 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Optional;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @Nested
+@DisplayName("Teste de Integração da Classe Item utilizando Mockito")
 @RunWith(MockitoJUnitRunner.class)
 @ExtendWith(MockitoExtension.class)
 public class ItemServiceMockTest {
@@ -26,25 +30,75 @@ public class ItemServiceMockTest {
     @InjectMocks // classe que sera testada
     private ItemService itemService;
 
-    @BeforeEach
-    void setUp() {
+    private Integer totalDeProdutos = 10;
+    private static final TipoCliente TIPO_CLIENTE = TipoCliente.A;
+    private Long idItemValido,idProdutoValido = 1l;
+
+    Produto produto = new Produto(this.idProdutoValido,"Caixa de Som",1000.00);
+    Item item = new Item(this.idItemValido,TIPO_CLIENTE,this.produto,2);
+
+    @Test
+    @DisplayName("Verificar que 10 produtos são retornados quando tenho 10 produtos cadastrados")
+    public void testFindAllQuandoTenho10ProdutosEntao10ProdutosSaoCarregados() {
+        when(this.itemRepository.getAll())
+                .thenReturn(criarItens(10,TIPO_CLIENTE));
+
+        Collection<Item> resultados = this.itemService.findAll();
+
+        assertEquals(this.totalDeProdutos, resultados.size());
     }
 
-    @AfterEach
-    void tearDown() {
+    @Test
+    @DisplayName("Verificar que nenhum produto é retornado quando o repositório está vazio")
+    public void testFindAllQuandoNenhumProdutoEntao0ProdutosSaoCarregados() {
+        when(this.itemRepository.getAll())
+                .thenReturn(criarItens(0,TIPO_CLIENTE));
+
+        Collection<Item> resultados = this.itemService.findAll();
+
+        assertEquals(0, resultados.size());
     }
+
 
     @Test
     public void testFindByIdQuandoIdProdutoInexistenteEntaoNenhumProdutoRetornado() {
         when(itemRepository.get(0l))
                 .thenReturn(Optional.empty());
-        // Procedimento
+
         Optional<Double> resultado = this.itemService.getValorComDesconto(0L);
-        // Verificação
+
         assertTrue(resultado.isEmpty());
         assertFalse(resultado.isPresent());
 
     }
 
+    @Test
+    @DisplayName("Verificar que o produto é retornado quando o identificador válido é utilizado")
+    public void testFindByIdQuandoIdProdutoValidoEntao1ProdutoRetornado() {
+
+        when(itemRepository.get(this.idItemValido))
+                .thenReturn(Optional.of(this.item));
+
+        Optional<Item> resultado = this.itemService.findById(this.idItemValido);
+
+        assertFalse(resultado.isEmpty());
+        assertTrue(resultado.isPresent());
+        assertNotNull(resultado.get());
+    }
+
+    private Collection<Item> criarItens(Integer quantidade, TipoCliente tipoCliente) {
+        Collection<Item> itens = new ArrayList<Item>();
+        Faker faker = new Faker(new Locale("pt_BR"));
+        Produto produto = new Produto(1l,faker.commerce().productName(), faker.number().randomDouble(2, 100, 2000));
+        for(int i = 0; i < quantidade; i++) {
+            itens.add(new Item(
+                    i+1l,
+                    tipoCliente,
+                    produto,
+                    faker.number().numberBetween(1, 1001)
+            ));
+        }
+        return itens;
+    }
 
 }
